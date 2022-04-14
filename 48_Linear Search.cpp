156d9431282779c1be2b9dfabd2074398e7ce2fc
manisha-7 <59906516+manisha-7@users.noreply.github.com>
2022-04-14 15:51:05 +0530

    Create 48_Linear Search.cpp

---
+++ 48_Linear Search.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void search(vector<int> arr, int search_Element)
+{
+	int left = 0;
+	int length = arr.size();
+	int position = -1;
+	int right = length - 1;
+	
+	for(left = 0; left <= right;)
+	{
+
+		if (arr[left] == search_Element)
+		{
+			
+			position = left;
+			cout << "Element found in Array at "
+				<< position + 1 << " Position with "
+				<< left + 1 << " Attempt";
+				
+			break;
+		}
+	
+		{
+			position = right;
+			cout << "Element found in Array at "
+				<< position + 1 << " Position with "
+				<< length - right << " Attempt";
+				
+			break;
+		}
+		left++;
+		right--;
+	}
+
+	if (position == -1)
+		cout << "Not found in Array with "
+			<< left << " Attempt";
+}
